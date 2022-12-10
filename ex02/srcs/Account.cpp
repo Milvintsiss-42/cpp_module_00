7e640ab3d95559e7cc9d70670612a2dc49f69739
Milvintsiss <milvintsiss@gmail.com>
2022-12-10 22:27:47 +0100

    Adds methods needed for the code to compile

--- ex02/srcs/Account.cpp
+++ ex02/srcs/Account.cpp
@@ -6,7 +6,83 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 21:33:51 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/12/10 21:33:57 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/12/10 22:17:06 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Account.hpp"
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(int initial_deposit)
+{
+	this->_accountIndex = Account::_nbAccounts;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_nbAccounts++;
+	makeDeposit(initial_deposit);
+}
+
+Account::~Account(void)
+{
+}
+
+int Account::getNbAccounts(void)
+{
+	return Account::_nbAccounts;
+}
+
+int Account::getTotalAmount(void)
+{
+	return Account::_totalAmount;
+}
+
+int Account::getNbDeposits(void)
+{
+	return Account::_totalNbDeposits;
+}
+
+int Account::getNbWithdrawals(void)
+{
+	return Account::_totalNbWithdrawals;
+}
+
+void Account::displayAccountsInfos(void)
+{
+}
+
+void Account::makeDeposit(int deposit)
+{
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+}
+
+bool Account::makeWithdrawal(int withdrawal)
+{
+	if (withdrawal > this->_amount)
+		return false;
+	this->_nbWithdrawals++;
+	Account::_totalNbWithdrawals++;
+	this->_amount -= withdrawal;
+	Account::_totalAmount -= withdrawal;
+	return true;
+}
+
+int Account::checkAmount(void) const
+{
+	return Account::_amount;
+}
+
+void Account::displayStatus(void) const
+{
+}
+
+void Account::_displayTimestamp(void)
+{
+}
